a138044e2c5c9ba5f908a9f7a244445eded4fa88
adrien-v <adrien.v@gmail.com>
2017-03-09 03:36:16 +0100

    add annotation entity and controller

--- cms/symphony3/src/AppBundle/Controller/AnnotationController.php
+++ cms/symphony3/src/AppBundle/Controller/AnnotationController.php
@@ -6,78 +6,82 @@
  * Time: 15:01
  */
 namespace AppBundle\Controller;
-use AppBundle\Form\TcPlaybackRateForm;
+use AppBundle\Form\AnnotationForm;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class TcPlaybackRateController extends Controller
+class AnnotationController extends Controller
 {
     /**
-     * @Route("/tcPlaybackRate", name="tcPlaybackRate")
+     * @Route("/annotation", name="annotation")
      */
     public function listAction()
     {
         $em = $this->getDoctrine()->getManager();
 
-        $tcs = $em->getRepository('AppBundle\Entity\TcPlaybackRate')
+        $tcs = $em->getRepository('AppBundle\Entity\Annotation')
             ->findAll();
 
         dump($tcs);//die;
 
-        return new Response('end listAction (TcPlaybackRateController)');
+        return new Response('end listAction (AnnotationController)');
     }
 
     /**
-     * @Route("/tcPlaybackRate/new", name="new_tcPlaybackRate")
+     * @Route("/annotation/new", name="new_annotation")
      */
     public function newAction(Request $request)
     {
-        $form = $this->createForm(TcPlaybackRateForm::class);
+        $form = $this->createForm(AnnotationForm::class,null,array(
+            'entity_manager' => $this->get('doctrine.orm.entity_manager')
+        ));
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            //dump($form->getData());die;
-            $tcPR = $form->getData();
 
+            $tcPR = $form->getData();
+            $tcPR -> setAnnotationType(
+                $tcPR->searchAnnotationType());
+            //dump($tcPR);die;
             $em = $this->getDoctrine()->getManager();
             $em->persist($tcPR);
             $em->flush();
-            return new Response("tcPlaybackRate created.<pre><code>".print_r($tcPR,true).'</code></pre>');
+            return new Response("annotation created.<pre><code>".print_r($tcPR,true).'</code></pre>');
         }
 
-        return $this->render('tc/playbackRate/new.html.twig', [
-            'TcPlaybackRateForm' => $form->createView()
+        return $this->render('annotation/new.html.twig', [
+            'AnnotationForm' => $form->createView()
         ]);
     }
 
     /**
-     * @Route("/tcPlaybackRate/{$id}/render", name="render_tcPlaybackRate")
+     * @Route("/annotation/{$id}/render", name="render_annotation")
      */
     public function renderAction($id)
     {
         $em = $this -> getDoctrine() -> getManager();
-        $tc = $em -> getRepository('AppBundle\Entity\TcPlaybackRate')
+        $tc = $em -> getRepository('AppBundle\Entity\Annotation')
             -> findOneBy(['id' => $id]);
         dump($tc);//die;
-        return new Response("end renderAction TcPlaybackRate ($id)");
+        return new Response("end renderAction Annotation ($id)");
     }
 
 
     /**
-     * @Route("/tcPlaybackRate/{$id}", name="show_tcPlaybackRate")
+     * @Route("/annotation/{$id}", name="show_annotation")
      */
     public function showAction($id)
     {
         $em = $this->getDoctrine()->getManager();
 
-        $tc = $em->getRepository('AppBundle\Entity\TcPlaybackRate')
+        $tc = $em->getRepository('AppBundle\Entity\Annotation')
             ->findOneBy(['id' => $id]);
 
         dump($tc);//die;
         //dump($media->searchMetadata());
-        return new Response('Show TcPlaybackRate.'  );
+        return new Response('Show Annotation.'  );
     }
 }
