0b727e5c34ed74120058790f39f34b93dfb7df62
adrien-v <adrien.v@gmail.com>
2017-03-09 03:37:01 +0100

    add Annotation entity

--- cms/symphony3/src/AppBundle/Entity/Annotation.php
+++ cms/symphony3/src/AppBundle/Entity/Annotation.php
@@ -8,13 +8,134 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
+
+/**
+ * Class Annotation
+ * @package AppBundle\Entity
+ * @ORM\Entity(repositoryClass="AnnotationRepository")
+ * @ORM\Table(name="annotation")
+ */
 class Annotation
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
     private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
     private $name;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\Choice(choices = {"srtFile", "csv_av", "json_avTV"}, message = "please use 'srtFile', 'csv_av' or 'json_avTV'")
+     */
+    // xxxx to move to integer : refid of the table AnnotationType
     private $annotationType;
 
-    private $segments;
+    /**
+     * @ORM\Column(type="string",nullable=true)
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
     private $media;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAnnotationType()
+    {
+        return $this->annotationType;
+    }
+
+    /**
+     * @param mixed $annotationType
+     */
+    public function setAnnotationType($annotationType)
+    {
+        $this->annotationType = $annotationType;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMedia()
+    {
+        return $this->media;
+    }
+
+    /**
+     * @param mixed $media
+     */
+    public function setMedia($media)
+    {
+        $this->media = $media;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param mixed $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function searchAnnotationType()
+    {
+        $urlPi = pathinfo($this->getUrl());
+        switch ($urlPi['extension']) {
+            case 'srt':
+                return 'srtFile';
+            case 'csv':
+                return 'csv_av';
+            case 'json':
+                return 'json_avTV';
+            default:
+                return '';
+        }
+    }
 }
